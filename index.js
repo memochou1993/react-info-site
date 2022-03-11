27bac6619413e18834686ce140ac179faa67441f
Memo Chou <memochou1993@gmail.com>
2022-03-11 15:12:18 +0800

    Add component

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const page = (
-  <div>
-    <img src="./react-logo.svg" width="40px" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on GitHub</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
+function MyPage() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src="./react-logo.svg" width="40px" />
+        </nav>
+      </header>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+      <footer>
+        <small>© 2022 Memo Chou</small>
+      </footer>
+    </div>
+  );
+};
 
-ReactDOM.render(page, document.getElementById('root'));
+ReactDOM.render(<MyPage />, document.getElementById('root'));
